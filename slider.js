310fce6be16f79d956a189e2170da33106f07e33
Oleksandra <sashkaburko1994@gmail.com>
2022-12-26 14:51:39 +0200

    1

--- slider.js
+++ slider.js
@@ -1,7 +1,21 @@
-const slider = document.getElementById('myRange');
-const output = document.getElementById('demo');
-output.innerHTML = slider.value;
+const allRanges = document.querySelectorAll('.range-wrap');
+allRanges.forEach(wrap => {
+  const range = wrap.querySelector('.slider');
+  const bubble = wrap.querySelector('.bubble');
 
-slider.oninput = function () {
-  output.innerHTML = this.value;
-};
+  range.addEventListener('input', () => {
+    setBubble(range, bubble);
+  });
+  setBubble(range, bubble);
+});
+
+function setBubble(range, bubble) {
+  const val = range.value;
+  const min = range.min ? range.min : 0;
+  const max = range.max ? range.max : 100;
+  const newVal = Number(((val - min) * 100) / (max - min));
+  bubble.innerHTML = val;
+
+  // Sorta magic numbers based on size of the native UI thumb
+  bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
+}
